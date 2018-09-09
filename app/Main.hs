c4bfda083295b8eef946cc7bfcb66c443a89fae1
Dustin Sallings <dustin@spy.net>
2018-09-08 21:35:26 -0700

    Added a CLI parser.

--- app/Main.hs
+++ app/Main.hs
@@ -7,27 +7,56 @@ import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM (newTChanIO)
 import Control.Monad (forever, forM_)
 import qualified Data.ByteString.Char8 as B
-import Data.Text (pack)
+import Network.Socket (HostName)
+import Network.Socket.Internal (PortNumber)
+import Data.Text (Text, isSuffixOf, pack)
+
+import Options.Applicative (option, auto, long, showDefault, value, help, helper, fullDesc,
+                            progDesc, execParser, info, str,
+                            (<**>), Parser)
 
 import System.Hardware.OneWire.Thermal
 import qualified Network.MQTT as MQTT
 
-base :: String
-base = "sj/attic/therm/"
+data Options = Options { optTopic :: Text
+                       , optHost :: HostName
+                       , optPort :: PortNumber
+                       , optUser :: Maybe Text
+                       , optPass :: Maybe Text
+                       , optClient :: Text
+                       , optPeriod :: Int
+                       }
 
-mktopic :: ThermalSerial -> MQTT.Topic
-mktopic (ThermalSerial s) = (MQTT.toTopic . MQTT.MqttText . pack) (base <> (s :: String))
+options :: Parser Options
+options = Options
+  <$> option str (long "topic" <> showDefault <> value "therm/" <>
+                   help "mqtt topic - if ends with a slash, sensor serial number will be appended")
+  <*> option str (long "host" <> showDefault <> value "localhost" <> help "mqtt host")
+  <*> option auto (long "port" <> showDefault <> value 1883 <> help "mqtt port")
+  <*> option auto (long "user" <> value Nothing <> help "mqtt username")
+  <*> option auto (long "pass" <> value Nothing <> help "mqtt password")
+  <*> option str (long "client" <> value "thermqtt" <> help "mqtt client name")
+  <*> option auto (long "period" <> showDefault <> value 5 <> help "time between readings")
 
-main :: IO ()
-main = do
+mktopic :: Options -> ThermalSerial -> MQTT.Topic
+mktopic opts (ThermalSerial s)
+  | "/" `isSuffixOf` optTopic opts = fromText $ optTopic opts <> pack s
+  | otherwise = fromText $ optTopic opts
+
+  where fromText = MQTT.toTopic . MQTT.MqttText
+
+go :: Options -> IO ()
+go opts = do
   cmds <- MQTT.mkCommands
   pubChan <- newTChanIO
   let conf = (MQTT.defaultConfig cmds pubChan)
              { MQTT.cClean = False
-             , MQTT.cClientID = "thermqtt"
-             , MQTT.cHost = "eve"
+             , MQTT.cClientID = optClient opts
+             , MQTT.cHost = optHost opts
+             , MQTT.cPort = optPort opts
+             , MQTT.cUsername = optUser opts
+             , MQTT.cPassword = optPass opts
              , MQTT.cKeepAlive = Just 10
-             , MQTT.cLogDebug = print
              }
 
   _ <- forkIO $ forever $ do
@@ -38,12 +67,18 @@ main = do
           maybe
             (pure ())
             (\c ->
-               MQTT.publish conf MQTT.NoConfirm False (mktopic serial) (B.pack $ show c)
+               MQTT.publish conf MQTT.NoConfirm False (mktopic opts serial) (B.pack $ show c)
             )
             mc)
 
-    threadDelay (1000000 * 60)
+    threadDelay (optPeriod opts * 1000000)
 
   -- this will throw IOExceptions
   terminated <- MQTT.run conf
   print terminated
+
+main :: IO ()
+main = execParser opts >>= go
+
+  where opts = info (options <**> helper)
+          ( fullDesc <> progDesc "Send MQTT therm updates")
